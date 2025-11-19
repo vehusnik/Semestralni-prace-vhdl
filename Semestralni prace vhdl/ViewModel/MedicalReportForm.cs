b640188a0e91d8f51ad28ffbf5f21d0fc557492e
Veronika Husníková <vehusnik@office365.zcu.cz>
2025-11-19 11:15:38 +0100

    version 02

---
+++ Semestralni prace vhdl/ViewModel/MedicalReportForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using DataEntity;
+using DataEntity.Data;
+
+namespace Semestralni_prace_vhdl.ViewModel
+{
+    public partial class MedicalReportForm : Window
+    {
+        private readonly int patientId;
+
+        public MedicalReportForm(int patientId)
+        {
+            InitializeComponent();
+            this.patientId = patientId;
+        }
+
+        // WPF uses RoutedEventArgs for button clicks
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Defensive: if date picker SelectedDate is null, use now
+            var examinationDate = dtpExaminationDate.SelectedDate ?? DateTime.Now;
+
+            var report = new Lekarskazprava
+            {
+                PacientId = patientId,
+                Datumvysetreni = examinationDate,
+                Symptomy = (Symptomy?.Text) ?? string.Empty,
+                Diagnoza = (txtDiagnosis?.Text) ?? string.Empty,
+                Doporuceni = (txtRecommendations?.Text) ?? string.Empty
+            };
+
+            using (var context = new LekarContext())
+            {
+                context.LekarskeZpravy.Add(report);
+                context.SaveChanges();
+            }
+
+            MessageBox.Show("Lékařská zpráva byla úspěšně uložena.", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            Close();
+        }
+    }
+}
