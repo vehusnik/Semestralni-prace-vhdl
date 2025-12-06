d0d4b8193cbee68ba07de3f0456d656e165444f4
domlampa-blip <domlampa@gapps.zcu.cz>
2025-12-06 17:55:29 +0100

    Fungují tabulky vetšina

---
+++ Semestralni prace vhdl/Views/NovyPacientWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DataEntity.Data;
+using System.Windows;
+
+namespace Semestralni_prace_vhdl.Views
+{
+    // OPRAVA 1: Třída se musí jmenovat stejně jako soubor -> NovyPacientWindow
+    public partial class NovyPacientWindow : Window
+    {
+        public Pacienti PacientData { get; set; }
+
+        // --- VARIANTA A: Nový pacient ---
+        public NovyPacientWindow()
+        {
+            InitializeComponent(); // Teď už to nebude svítit červeně
+            PacientData = new Pacienti();
+            this.DataContext = PacientData;
+            Title = "Přidat nového pacienta";
+        }
+
+        // --- VARIANTA B: Úprava pacienta ---
+        public NovyPacientWindow(Pacienti pacientKUprave)
+        {
+            InitializeComponent();
+            PacientData = pacientKUprave;
+            this.DataContext = PacientData;
+            Title = "Upravit existujícího pacienta";
+        }
+
+        // --- Tlačítko ULOŽIT ---
+        private void BtnUlozit_Click(object sender, RoutedEventArgs e)
+        {
+            // OPRAVA 2: Tady jste měl překlep "PacientiData". Správně je "PacientData".
+            if (PacientData.Id == 0)
+            {
+                // INSERT (Nový)
+                // Zde doplňte volání databáze, např.: _context.Pacienti.Add(PacientData); _context.SaveChanges();
+                MessageBox.Show("Nový pacient byl přidán.");
+            }
+            else
+            {
+                // UPDATE (Existující)
+                // Zde doplňte volání databáze, např.: _context.SaveChanges();
+                MessageBox.Show("Změny byly uloženy.");
+            }
+
+            this.Close();
+        }
+    }
+}
---
+++ Semestralni prace vhdl/Views/PřehledPacientuView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DataEntity.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Semestralni_prace_vhdl.Views
+{
+    public partial class PřehledPacientuView : Window
+    {
+        public PřehledPacientuView()
+        {
+            InitializeComponent();
+        }
+
+        // TOTO JE TA METODA PRO TLAČÍTKO PŘIDAT
+        private void PridatPacienta_Click(object sender, RoutedEventArgs e)
+        {
+            // Vytvoříme instanci okna pro nového pacienta
+            NovyPacientWindow formular = new NovyPacientWindow();
+
+            // Otevřeme ho.
+            // ShowDialog() je lepší než Show(), protože nedovolí klikat do seznamu,
+            // dokud tento formulář nezavřeš nebo neuložíš.
+            formular.ShowDialog();
+        }
+        private void PridatZpravu_Click(object sender, RoutedEventArgs e)
+        {
+            // Otevřeme okno lékařské zprávy
+            LekarskaZpravaWindow zpravaOkno = new LekarskaZpravaWindow();
+            zpravaOkno.ShowDialog();
+        }
+        private void BtnUpravit_Click(object sender, RoutedEventArgs e)
+        {
+            // Získáme tlačítko, na které se kliklo
+            Button btn = (Button)sender;
+
+            // Získáme data z řádku (předpokládám, že vaše třída se jmenuje 'Pacient')
+            var vybranyPacient = btn.DataContext as Pacienti;
+
+            if (vybranyPacient != null)
+            {
+                // Zde otevřete okno pro úpravu a pošlete tam data
+                // Příklad:
+                // OknoUpravy okno = new OknoUpravy(vybranyPacient);
+                // okno.Show();
+
+                MessageBox.Show("Upravuji pacienta: " + vybranyPacient.Prijmeni); // Prozatímní test
+            }
+        }
+    }
+}
