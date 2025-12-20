aae486c1dd15b65ffcc665558e2e5bc60408714e
domlampa-blip <domlampa@gapps.zcu.cz>
2025-12-21 00:13:30 +0100

    Revert "Delete DataEntity directory"
    This reverts commit ff00a0b3885e5e8ef4524d4437a8922c6afdf522.

---
+++ DataEntity/Migrations/20251220211835_ChangeTitlesToString.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataEntity.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTitlesToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulza",
+                table: "Pacienti",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulpřed",
+                table: "Pacienti",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Titulza",
+                table: "Pacienti",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Titulpřed",
+                table: "Pacienti",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
